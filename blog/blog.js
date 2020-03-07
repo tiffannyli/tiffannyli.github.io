a2a65edaa98784e11a8e9fa20d3f7ae3033d3efd
vitasoyy <46633833+vitasoyy@users.noreply.github.com>
2020-03-07 00:12:26 -0500

    new blog section

---
+++ blog/blog.js
@@ -0,0 +1,24 @@
+function buttonfunction() {
+    var x = document.getElementById("panel1");
+    if (x.style.display == 'none' || x.style.display == "") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+function buttonfunction2() {
+    var y = document.getElementById("panel2");
+    if (y.style.display == 'none' || y.style.display == "") {
+      y.style.display = "block";
+    } else {
+      y.style.display = "none";
+    }
+  }
+function buttonfunction3() {
+    var z = document.getElementById("panel3");
+    if (z.style.display == 'none' || z.style.display == "") {
+      z.style.display = "block";
+    } else {
+      z.style.display = "none";
+    }
+  }
