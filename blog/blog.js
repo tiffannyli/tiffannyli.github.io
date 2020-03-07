4b31aaba90ee158037ac7d0d410810e4e7118192
vitasoyy <46633833+vitasoyy@users.noreply.github.com>
2020-03-07 01:30:51 -0500

    added icons

--- blog/blog.js
+++ blog/blog.js
@@ -1,24 +1,29 @@
-function buttonfunction() {
+
+
+function buttonfunction(e) {
     var x = document.getElementById("panel1");
     if (x.style.display == 'none' || x.style.display == "") {
       x.style.display = "block";
     } else {
       x.style.display = "none";
     }
+    e.classList.toggle("toggled");
   }
-function buttonfunction2() {
+function buttonfunction2(e) {
     var y = document.getElementById("panel2");
     if (y.style.display == 'none' || y.style.display == "") {
       y.style.display = "block";
     } else {
       y.style.display = "none";
     }
+    e.classList.toggle("toggled");
   }
-function buttonfunction3() {
+function buttonfunction3(e) {
     var z = document.getElementById("panel3");
     if (z.style.display == 'none' || z.style.display == "") {
       z.style.display = "block";
     } else {
       z.style.display = "none";
     }
+    e.classList.toggle("toggled");
   }
